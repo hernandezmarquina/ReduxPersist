4558c2487ac54fb7cb63f36c9c790b16cdec046d
Jonathan Hernandez <jonathan.hernandez@bowheadhealth.com>
2023-09-02 23:35:05 -0600

    Persist only the evaluation reducer

--- src/store/index.ts
+++ src/store/index.ts
@@ -17,6 +17,7 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 const persistConfig = {
   key: 'root',
   storage: AsyncStorage,
+  whitelist: ['evaluation'],
 };
 
 const rootReducer = combineReducers({
